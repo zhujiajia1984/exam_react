a88e082eb4314ffc07a25bb254eac04fc3c149e5
zhujiajia <772978409@qq.com>
2017-11-15 16:28:37 +0800

    初始化

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 18500,
+        historyApiFallback: true,
+    },
+    devtool: 'source-map', // source-map
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            }
+        }, {
+            test: /\.css$/,
+            use: [
+                { loader: "style-loader" },
+                { loader: "css-loader" },
+            ]
+        }, ]
+    }
+}
