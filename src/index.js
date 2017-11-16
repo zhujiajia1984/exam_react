c8615bb7b249a5b19493d5a7279e01523782019c
zhujiajia <772978409@qq.com>
2017-11-16 11:49:21 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,125 +1,167 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { NavBar, Toast, List, WhiteSpace, Radio, Button, WingBlank } from 'antd-mobile';
+import {
+	NavBar,
+	Toast,
+	WhiteSpace,
+	Button,
+	WingBlank,
+	Result,
+	Icon
+} from 'antd-mobile';
 import indexCss from './index.css';
-
-const RadioItem = Radio.RadioItem;
+import Question from './Question.js'
 
 const title = [
 	"Q1：根据建设部的有关规定，施工单位（   ）的工人，必须接受三级安全培训教育，经考核合格后，方能上岗。",
 	"Q2：安全生产管理是实现安全生产的重要（   ）。",
 	"Q3：安全是（   ）"
 ]
-const data1 = [
-	{ value: "A", label: '转岗' },
-	{ value: "B", label: '新入场' },
-	{ value: "C", label: '变换工种' },
-	{ value: "D", label: '从事特种作业' },
+const data = [
+	[
+		{ value: "A", label: '转岗' },
+		{ value: "B", label: '新入场' },
+		{ value: "C", label: '变换工种' },
+		{ value: "D", label: '从事特种作业' },
+	],
+	[
+		{ value: "A", label: '作用' },
+		{ value: "B", label: '保证' },
+		{ value: "C", label: '依据' },
+		{ value: "D", label: '措施' },
+	],
+	[
+		{ value: "A", label: '没有危险的状态' },
+		{ value: "B", label: '没有事故的状态' },
+		{ value: "C", label: '舒适的状态' },
+		{ value: "D", label: '生产系统中人员免遭不可承受危险的伤害' },
+	]
 ];
-const data2 = [
-	{ value: "A", label: '作用' },
-	{ value: "B", label: '保证' },
-	{ value: "C", label: '依据' },
-	{ value: "D", label: '措施' },
-];
-
-const data3 = [
-	{ value: "A", label: '没有危险的状态' },
-	{ value: "B", label: '没有事故的状态' },
-	{ value: "C", label: '舒适的状态' },
-	{ value: "D", label: '生产系统中人员免遭不可承受危险的伤害' },
-];
-
+const answer = ["B", "B", "D"];
 //
 class Main extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value1: "",
-			data1: [],
-			data2: [],
-			data3: [],
+			value: ["", "", "", ],
+			data: [],
 			title: [],
 			isShowBtn: false,
 		}
 	}
 	//
 	componentDidMount() {
-		Toast.loading("数据加载中", 0);
+		Toast.loading("试题加载中", 0);
 		setTimeout(() => {
 			Toast.hide();
 			this.setState({
-				data1: data1,
-				data2: data2,
-				data3: data3,
+				data: data,
 				title: title,
-				isShowBtn: true,
+				isShowPage: true,
+				isShowCorrect: false,
+				isShowWrong: false,
+				wrongNumber: 0,
 			})
 		}, 2000)
 	}
 	//
-	onChange1(value, e) {
-		this.setState({ value1: value })
+	onChange(index, value) {
+		let values = this.state.value;
+		values[index] = value;
+		this.setState({ value: values })
 	}
-	onChange2(value, e) {
-		this.setState({ value2: value })
-	}
-	onChange3(value, e) {
-		this.setState({ value3: value })
+	//
+	submitAnswer() {
+		//是否答题
+		let noAnswer = 0;
+		for (let i = 0; i < this.state.value.length; i++) {
+			if (this.state.value[i] == "")
+				noAnswer++
+		}
+		if (noAnswer > 0) {
+			Toast.info("请先进行答题", 2);
+			return;
+		}
+		//是否有错
+		let wrongs = 0;
+		for (let i = 0; i < this.state.value.length; i++) {
+			if (answer[i] != this.state.value[i]) {
+				wrongs++
+			}
+		}
+		if (wrongs > 0) {
+			// 有错
+			this.setState({
+				isShowPage: false,
+				isShowCorrect: false,
+				isShowWrong: true,
+				wrongNumber: wrongs,
+			})
+			Toast.loading("试题加载中", 0);
+			setTimeout(() => {
+				Toast.hide();
+				this.setState({
+					data: data,
+					title: title,
+					value: ["", "", "", ],
+					isShowPage: true,
+					isShowCorrect: false,
+					isShowWrong: false,
+					wrongNumber: 0,
+				})
+			}, 4000)
+		} else {
+			this.setState({
+				isShowPage: false,
+				isShowCorrect: true,
+				isShowWrong: false,
+			})
+		}
 	}
 	//
 	render() {
+		const content =
+			<div>
+				{this.state.data.map((item,index) => (
+					<Question key={index}
+						title={this.state.title[index]}
+						data={this.state.data[index]}
+						value={this.state.value[index]}
+						onChange={this.onChange.bind(this, index)}
+					>
+					</Question>
+				))}
+				<WingBlank>
+					<Button type="primary" 
+							onClick={this.submitAnswer.bind(this)}
+					>提交</Button>
+				</WingBlank>
+				<WhiteSpace size="lg"></WhiteSpace>
+			</div>
+		const correct =
+			<div>
+				<Result
+					img={<Icon type="check-circle" 
+								style={{ fill: '#1F90E6', width: '60px', height:'60px'}} />}
+					title="答题成功"
+					message="恭喜你，已可以正常上网！"
+				/>
+			</div>
+		const wrong =
+			<div>
+				<Result
+					img={<Icon type="cross-circle-o" 
+								style={{ fill: '#F13642', width: '60px', height:'60px'}} />}
+					title="答题失败"
+					message={`您有${this.state.wrongNumber}道题答错了,请重新答题`}
+				/>
+			</div>
 		return (
 			<div>
 				<NavBar mode="dark">测试问卷</NavBar>
-				<WhiteSpace size="lg"></WhiteSpace>
-				<span style={{lineHeight: '25px'}}>{this.state.title[0]}</span>
-				<WhiteSpace size="md"></WhiteSpace>
-				<List>
-					{this.state.data1.map(item => (
-						<RadioItem 
-							key={item.value}
-							checked={this.state.value1 === item.value}
-							onChange={this.onChange1.bind(this, item.value)}
-						>
-							{item.value + "：" + item.label}
-						</RadioItem>
-					))}
-				</List>
-				<WhiteSpace size="lg"></WhiteSpace>
-				<span style={{lineHeight: '25px'}}>{this.state.title[1]}</span>
-				<WhiteSpace size="md"></WhiteSpace>
-				<List>
-					{this.state.data2.map(item => (
-						<RadioItem 
-							key={item.value}
-							checked={this.state.value2 === item.value}
-							onChange={this.onChange2.bind(this, item.value)}
-						>
-							{item.value + "：" + item.label}
-						</RadioItem>
-					))}
-				</List>
-				<WhiteSpace size="lg"></WhiteSpace>
-				<span style={{lineHeight: '25px'}}>{this.state.title[2]}</span>
-				<WhiteSpace size="md"></WhiteSpace>
-				<List>
-					{this.state.data3.map(item => (
-						<RadioItem 
-							key={item.value}
-							checked={this.state.value3 === item.value}
-							onChange={this.onChange3.bind(this, item.value)}
-						>
-							{item.value + "：" + item.label}
-						</RadioItem>
-					))}
-				</List>
-				{(this.state.isShowBtn)
-					?<WingBlank>
-						<Button type="primary">提交</Button>
-					</WingBlank>:<WhiteSpace size="xs"></WhiteSpace>
-				}
-				<WhiteSpace size="lg"></WhiteSpace>
+				{(this.state.isShowPage)?content:""}
+				{(this.state.isShowCorrect)?correct:""}
+				{(this.state.isShowWrong)?wrong:""}
 			</div>
 		);
 	}
